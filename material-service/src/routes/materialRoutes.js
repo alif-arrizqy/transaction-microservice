e5bce7f6ee9955466474ac3aa2e8a9445a16bf61
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-07-27 22:15:55 +0700

    add: create new routes

--- material-service/src/routes/materialRoutes.js
+++ material-service/src/routes/materialRoutes.js
@@ -5,5 +5,8 @@ const materialController = require("../controllers/materialController");
 // Define your CRUD routes here
 router.post("/", materialController.createMaterial);
 router.get("/", materialController.getAllMaterials);
+router.get("/:id", materialController.getMaterial);
+router.put("/:id", materialController.updateMaterial);
+router.delete("/:id", materialController.deleteMaterial);
 
 module.exports = router;
--- transaction-service/src/routes/transactionRoutes.js
+++ transaction-service/src/routes/transactionRoutes.js
@@ -5,6 +5,8 @@ const transactionController = require("../controllers/transactionController");
 // Define your CRUD routes here
 router.post("/", transactionController.createTransaction);
 router.get("/", transactionController.getAllTransactions);
-
+router.get("/:id", transactionController.getTransaction);
+router.put("/:id", transactionController.updateTransaction);
+router.delete("/:id", transactionController.deleteTransaction);
 
 module.exports = router;
