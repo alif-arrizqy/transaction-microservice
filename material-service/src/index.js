b7c81605529e0414fab8b35de254444b6992f32b
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-07-27 22:17:10 +0700

    update: add message subscriber for RabbitMQ pub/sub

--- material-service/src/index.js
+++ material-service/src/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const sequelize = require("../src/config/config");
 const materialRoutes = require("../src/routes/materialRoutes");
+const MessageSubscriber = require("../src/messaging/subscriber");
+
 require("dotenv").config();
+
+const subscriber = new MessageSubscriber();
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,6 +19,7 @@ sequelize
   .sync()
   .then(() => {
     app.listen(`${PORT}`, () => {
+      subscriber.subscribeMessages();
       console.log(`Server is running on port ${PORT}`);
     });
   })
--- user-service/src/index.js
+++ user-service/src/index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const sequelize = require("../src/config/config");
 const dotenv = require("dotenv");
 const userRoutes = require("../src/routes/userRoutes");
+const MessageSubscriber = require("../src/messaging/subscriber");
+
+const subscriber = new MessageSubscriber();
 
 const app = express();
 dotenv.config();
@@ -17,6 +20,7 @@ sequelize
   .sync()
   .then(() => {
     app.listen(`${PORT}`, () => {
+      subscriber.subscribeMessages();
       console.log(`Server is running on port ${PORT}`);
     });
   })
