3e3172601a51140e456b6e266e9b0adf68a645a7
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-07-27 22:13:01 +0700

    fix: typo in mysql username

--- user-service/src/config/config.js
+++ user-service/src/config/config.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const sequelize = new Sequelize(
   process.env.MYSQL_DATABASE,
-  process.env.USER,
+  process.env.MYSQL_USERNAME,
   process.env.MYSQL_PASSWORD,
   {
     host: process.env.MYSQL_HOST,
